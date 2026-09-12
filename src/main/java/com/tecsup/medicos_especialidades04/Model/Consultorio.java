7604b7917784b20d0b752aa8071f12e17b3dc710
Programer-DMeli <carbajalmeli369@gmail.com>
2026-09-12 03:19:34 -0500

    Feat: Agregue horarios de atencion y consultorio

--- src/main/java/com/tecsup/medicos_especialidades04/Model/Consultorio.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Model/Consultorio.java
@@ -1,4 +1,108 @@
 package com.tecsup.medicos_especialidades04.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "consultorios")
 public class Consultorio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String codigo;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Integer piso;
+
+    @Column(nullable = false, length = 100)
+    private String area;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private EstadoConsultorio estado;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "especialidad_id", nullable = false)
+    private Especialidades especialidad;
+
+   //Constructor sin parametros
+
+    public Consultorio() {
+    }
+    //Constructor con parametros
+    public Consultorio(String codigo, String nombre, Integer piso,
+                       String area, EstadoConsultorio estado,
+                       Especialidades especialidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.piso = piso;
+        this.area = area;
+        this.estado = estado;
+        this.especialidad = especialidad;
+    }
+
+    // ==============================
+    // GETTERS Y SETTERS
+    // ==============================
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPiso() {
+        return piso;
+    }
+
+    public void setPiso(Integer piso) {
+        this.piso = piso;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public EstadoConsultorio getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoConsultorio estado) {
+        this.estado = estado;
+    }
+
+    public Especialidades getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidades especialidad) {
+        this.especialidad = especialidad;
+    }
 }
--- src/main/java/com/tecsup/medicos_especialidades04/Model/HorarioAtencion.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Model/HorarioAtencion.java
@@ -1,4 +1,104 @@
 package com.tecsup.medicos_especialidades04.Model;
 
+import jakarta.persistence.*;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "horario_atencion")
 public class HorarioAtencion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_horario")
+    private Long idHorario;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id", nullable = false)
+    private Medico medico;
+
+    @ManyToOne
+    @JoinColumn(name = "consultorio_id", nullable = false)
+    private Consultorio consultorio;
+
+    @Column(name = "dia_semana", nullable = false, length = 20)
+    private String diaSemana;
+
+    @Column(name = "hora_inicio", nullable = false)
+    private LocalTime horaInicio;
+
+    @Column(name = "hora_fin", nullable = false)
+    private LocalTime horaFin;
+
+    @Column(name = "duracion_cita", nullable = false)
+    private Integer duracionCita;
+
+    @Column(name = "estado", nullable = false)
+    private String estado;
+
+    public HorarioAtencion() {
+    }
+
+    // getters y setters
+
+    public Long getIdHorario() {
+        return idHorario;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public Consultorio getConsultorio() {
+        return consultorio;
+    }
+
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public LocalTime getHoraFin() {
+        return horaFin;
+    }
+
+    public Integer getDuracionCita() {
+        return duracionCita;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setIdHorario(Long idHorario) {
+        this.idHorario = idHorario;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public void setConsultorio(Consultorio consultorio) {
+        this.consultorio = consultorio;
+    }
+
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public void setHoraFin(LocalTime horaFin) {
+        this.horaFin = horaFin;
+    }
+    public void setDuracionCita(Integer duracionCita) {
+        this.duracionCita = duracionCita;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
