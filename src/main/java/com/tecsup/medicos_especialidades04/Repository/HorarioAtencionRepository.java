194e7d920679291337a1002c79a2d762810625cc
mayragarcia-debug <mayra.garcia@tecsup.edu.pe>
2026-09-12 04:18:12 -0500

    feat: Actualización del repositorio de horarios de atención

--- src/main/java/com/tecsup/medicos_especialidades04/Repository/HorarioAtencionRepository.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Repository/HorarioAtencionRepository.java
@@ -1,4 +1,9 @@
 package com.tecsup.medicos_especialidades04.Repository;
 
-public interface HorarioAtencionRepository {
+import com.tecsup.medicos_especialidades04.Model.HorarioAtencion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HorarioAtencionRepository extends JpaRepository<HorarioAtencion, Long> {
 }
