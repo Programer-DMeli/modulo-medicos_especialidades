c89d62c91f52b70ec6e4a1fca17951fe1c8e30d9
mayragarcia-debug <mayra.garcia@tecsup.edu.pe>
2026-09-12 04:18:35 -0500

    feat: Actualización del repositorio de médicos

--- src/main/java/com/tecsup/medicos_especialidades04/Repository/MedicoRepository.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Repository/MedicoRepository.java
@@ -2,6 +2,8 @@ package com.tecsup.medicos_especialidades04.Repository;
 
 import com.tecsup.medicos_especialidades04.Model.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 }
