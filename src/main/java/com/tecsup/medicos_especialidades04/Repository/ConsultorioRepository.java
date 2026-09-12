9d704aeb183dceac2f843d6847e6e864051a202f
mayragarcia-debug <mayra.garcia@tecsup.edu.pe>
2026-09-12 04:17:51 -0500

    feat: Actualización del repositorio de consultorios

--- src/main/java/com/tecsup/medicos_especialidades04/Repository/ConsultorioRepository.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Repository/ConsultorioRepository.java
@@ -1,4 +1,9 @@
 package com.tecsup.medicos_especialidades04.Repository;
 
-public interface ConsultorioRepository {
+import com.tecsup.medicos_especialidades04.Model.Consultorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultorioRepository extends JpaRepository<Consultorio, Long> {
 }
