1e8c86d57aabdbb57a735397a594ca608996d892
mayragarcia-debug <mayra.garcia@tecsup.edu.pe>
2026-09-12 04:34:18 -0500

    feat: Implementación del controlador de especialidades

--- src/main/java/com/tecsup/medicos_especialidades04/Controller/EspecialidadesController.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Controller/EspecialidadesController.java
@@ -1,4 +1,49 @@
 package com.tecsup.medicos_especialidades04.Controller;
 
+import com.tecsup.medicos_especialidades04.Model.Especialidades;
+import com.tecsup.medicos_especialidades04.Service.EspecialidadesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/especialidades")
 public class EspecialidadesController {
+
+    @Autowired
+    private EspecialidadesService service;
+
+    // Listar especialidades
+    @GetMapping
+    public List<Especialidades> listar() {
+        return service.listar();
+    }
+
+    // Obtener especialidad por ID
+    @GetMapping("/{id}")
+    public Especialidades obtener(@PathVariable Long id) {
+        return service.obtener(id);
+    }
+
+    // Registrar especialidad
+    @PostMapping
+    public Especialidades guardar(@RequestBody Especialidades especialidad) {
+        return service.guardar(especialidad);
+    }
+
+    //Activar o desactivar especialidad
+    @PutMapping("/{id}/estado")
+    public Especialidades cambiarEstado(
+            @PathVariable Long id,
+            @RequestParam Boolean estado) {
+
+        return service.cambiarEstado(id, estado);
+    }
+
+    // Eliminar especialidad
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        service.eliminar(id);
+    }
 }
